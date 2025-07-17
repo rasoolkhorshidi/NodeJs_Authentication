2c67d817dbec268d1996ffb6f7913be133374593
rasoolkhorshidi <rasool.khorshidi@gmail.com>
2025-07-17 11:37:55 +0330

    Save Changes

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const router = require("./src/routes/index");
 const mongoose = require("mongoose");
 const debug = require("debug")("app:main");
 const config = require("config");
+const { User } = require("./src/routes/models/user"); // Assuming you have a User model
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use("/api", router);
-
+// Connect to MongoDB
 mongoose
   .connect(config.get("database.url"))
   .then(() => {
@@ -19,6 +19,23 @@ mongoose
   .catch((err) => {
     debug("Error connecting to MongoDB:", err);
   });
+
+app.use("/api", router);
+
+app.get("/", (req, res) => {
+  // async function to show all users
+  async function showUsers() {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error fetching users", error: error.message });
+    }
+  }
+  showUsers();
+});
 
 app.listen(config.get("server.port"), () => {
   debug(`Server is running on port ${config.get("server.port")}`);
---
+++ src/routes/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+//create a schema for user
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minlength: 3,
+    maxlength: 20,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+//create a model for user
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
